75dffe1a5d4472a7115059de3793231371c32806
Andrey Maximov <arman.gen40@mail.ru>
2024-10-29 20:04:50 +0300

    Add components, change App.jsx

---
+++ vite-project/src/components/ProjectList/ProjectList.jsx
@@ -0,0 +1,14 @@
+import './ProjectList.css';
+
+function ProjectList ({ projects }) {
+    let index = 0;
+
+    return (
+        projects.map((item) => (
+            <div className='project__img' key={index++}>
+                <img src={item.img} alt='' />
+            </div>
+        ))
+    );
+}
+export default ProjectList
